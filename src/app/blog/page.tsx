b66f0d2c8e775a4ce26ade96fc39a905d245253d
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-15 13:18:09 +0500

    Metadata title Done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next';
+export const metadata:Metadata = {
+    title: {
+        absolute:"Blog Page", // escape from parent meta title's template
+    },
+}
 export  default  function BlogPage(){
     return <h1>My Blog</h1>
 }
