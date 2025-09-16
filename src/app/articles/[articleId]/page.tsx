3ce708f281de7c71aa3011cbae60aa979a0f5683
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-16 13:20:15 +0500

    Params and searchParams for Client Component Done

--- src/app/articles/[articleId]/page.tsx
+++ src/app/articles/[articleId]/page.tsx
@@ -1,21 +1,22 @@
+"use client"
 import Link from "next/link";
-
+import {use} from "react"
 interface Props{
     params: Promise<{articleId:string}>
     searchParams: Promise<{lang?: "en" | "es" | "fr"}>
 }
-export default async function ArticleDetailPage({params, searchParams}: Props)  {
-    const {articleId} = await params;
-    const {lang = "en"} = await searchParams;
+export default function ArticleDetailPage({params, searchParams}: Props)  {
+    const {articleId} = use(params);
+    const {lang = "en"} = use(searchParams);
     return (
         <div>
             <h1>News Articles {articleId}</h1>
             <p>Reading in Language {lang}</p>
 
             <div>
-                <Link href={`/articles/${articleId}?lang=${lang}`}>English</Link>
-                <Link href={`/articles/${articleId}?lang=${lang}`}>Spanish</Link>
-                <Link href={`/articles/${articleId}?lang=${lang}`}>French</Link>
+                <Link href={`/articles/${articleId}?lang=en`}>English</Link>
+                <Link href={`/articles/${articleId}?lang=fs`}>Spanish</Link>
+                <Link href={`/articles/${articleId}?lang=fr`}>French</Link>
             </div>
         </div>
     )
