7c8ad7bd01d67f0e0a221d3cc33761b7f192c74f
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-18 12:32:19 +0500

    Error.tsx and Error Boundary Done

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,8 @@
 import {redirect} from "next/navigation";
 
+const generateRandomInt = (count: number) => {
+    return Math.floor(Math.random() * count);
+}
 export default async function ReviewDetailPage({params}:{
     params:Promise<{
         productId:string;
@@ -7,6 +10,11 @@ export default async function ReviewDetailPage({params}:{
     }>
 }) {
     const {productId, reviewId} = await params;
+    const random = generateRandomInt(2);
+    if(random === 1){
+        throw new Error("Error loading review")
+    }
+
 
     if(parseInt(reviewId) > 1000){
         redirect('/');
