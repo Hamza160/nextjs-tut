b6acde65e3a2b0ad816e987280729507bb2ca054
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-18 12:20:14 +0500

    loading done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,12 @@
-import type { Metadata } from 'next';
-export const metadata:Metadata = {
+import type {Metadata} from 'next';
+
+export const metadata: Metadata = {
     title: {
-        absolute:"Blog Page", // escape from parent meta title's template
+        absolute: "Blog Page", // escape from parent meta title's template
     },
 }
 
-export  default  function BlogPage(){
+export default async function BlogPage() {
+    await new Promise(resolve => setTimeout(resolve, 2000));
     return <h1>My Blog</h1>
 }
