7d1f0a133bd2fa0fe3680c220735bc725e9f3793
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-09-25 12:16:20 +0500

    Headers in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,16 @@
-export async function GET()
+import {NextRequest} from "next/server";
+import {headers} from "next/headers";
+export async function GET(req: NextRequest)
 {
-    return new Response('Hello Profile!');
+    // const requestHeaders = new Headers(req.headers);
+    // console.log(requestHeaders.get('Authorization'));
+
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"));
+
+    return new Response('<h1>Hello Profile!</h1>', {
+        headers: {
+            'Content-type': 'text/html',
+        }
+    });
 }
