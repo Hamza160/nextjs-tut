85d5157b8c17c99433516231b3f73553210e402d
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-09-25 12:24:46 +0500

    Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import {NextRequest} from "next/server";
-import {headers} from "next/headers";
+import {headers, cookies} from "next/headers";
 export async function GET(req: NextRequest)
 {
     // const requestHeaders = new Headers(req.headers);
@@ -8,9 +8,17 @@ export async function GET(req: NextRequest)
     const headerList = await headers();
     console.log(headerList.get("Authorization"));
 
+    const theme = req.cookies.get("theme")
+    console.log(theme);
+
+    const cookieStore = await cookies()
+    cookieStore.set('resultPerPage', "20")
+    console.log(cookieStore.get('resultPerPage'))
+    // TODO:: Cookie More Methods Has and Delete
     return new Response('<h1>Hello Profile!</h1>', {
         headers: {
             'Content-type': 'text/html',
+            'Set-Cookie': 'theme=dark'
         }
     });
 }
