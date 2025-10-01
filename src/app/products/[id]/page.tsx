e505c8e5bb6e4d593de46670716917ad0416d530
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-01 12:48:43 +0500

    SSG Static Site Generation Done

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,14 @@
+export async function generateStaticParams() {
+    return [
+        {id:"1"},
+        {id:"2"},
+        {id:"3"},
+        {id:"4"},
+    ]
+}
+export default async function ProductPage({params}:{params:Promise<{id:string}>})  {
+    const {id} = await params
+    return (
+        <h1>Product Details For Product {id} {new Date().toLocaleTimeString()}</h1>
+    )
+}
