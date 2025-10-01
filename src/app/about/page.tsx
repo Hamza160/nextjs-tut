e350353e8bf4502997eecee6588249c2b5498f4d
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-01 12:46:50 +0500

    Dynamic Rendering Done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
-export default function AboutPage() {
+import {cookies} from 'next/headers'
+
+export default async function AboutPage() {
+    const coookieStore= await cookies()
+    const theme = coookieStore.get("theme")
+    console.log(theme)
     return <h1>About Page {new Date().toLocaleTimeString()}</h1>
 }
