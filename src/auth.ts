fc375a1ce6542454fb084602a5123c150416002e
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-01 12:55:26 +0500

    Google and Github Login With Consent

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,31 @@ import GithubProvider from "next-auth/providers/github"
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
     providers: [
-        GoogleProvider,
-        GithubProvider
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+            // Optional If You are not using any database
+            authorization:{
+                params:{
+                    prompt:"consent", // Everytime you try to authenticate using google you will be asked a consent that you want to continue or not
+                    access_type:"offline",
+                    response_type:"code",
+                }
+            }
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+
+            // Optional If You are not using any database
+            authorization:{
+                params:{
+                    prompt:"consent", // Everytime you try to authenticate using google you will be asked a consent that you want to continue or not
+                    access_type:"offline",
+                    response_type:"code",
+                }
+            }
+        })
     ],
 })
