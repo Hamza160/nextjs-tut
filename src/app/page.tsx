7658c33b9c8b840d98d18165f321dfbfcd035a5e
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-06 12:09:12 +0500

    React Query Client Side Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,44 +2,26 @@
 import Link from "next/link";
 import {useQuery} from "@tanstack/react-query";
 
-async function getData() {
-    const options = {
-        method: "GET",
-        headers: {
-            accept: "application/json",
-        },
-    };
-
-    const response = fetch(
-        "https://jsonplaceholder.typicode.com/todos",
-        options
-    )
-        .then((response) => response.json())
-        .catch((err) => console.error(err));
-
-    return response;
-}
-
-async function getMovies() {
-    const data = await getData();
-    return data;
+async function fetchPosts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return await response.json();
 }
 
 export default function HomePage() {
-    const {data, isLoading, isError} = useQuery({
-        queryKey: ["movies"],
-        queryFn: async () => await getMovies(),
-    })
+    const {data, error, isLoading, isError} = useQuery({
+        queryKey: ['posts'],
+        queryFn: fetchPosts,
+    });
 
-    if (isLoading) return <h1>Loading....</h1>;
-    if (isError) return <div>Sorry There was an Error</div>;
+    if (isLoading) return <h1>Loading....</h1>
+    if (isError) return <h1>Error: {(error as Error).message}</h1>
 
     return (
         <div>
             <Link href="/dashboard">Dashboard</Link>
             <Link href="/about">About Page</Link>
-            {data.map((item) => (
-                <h3 key={item.id}>{item.title}</h3>
+            {data && data.length > 0 && data.map(post => (
+                <h3 key={post.id}>{post.title}</h3>
             ))}
         </div>
     )
