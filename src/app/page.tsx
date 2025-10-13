f63b366f1ad41e1a6fd0d80735ebefedf4f2d064
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-13 12:25:32 +0500

    NextJs vs ReactJs Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
+"use client"
 
-export default function HomePage (){
-    return (
-        <div>
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/about">About Page</Link>
-            <h1>Hello From Home Page</h1>
-        </div>
-    )
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>Hello World</div>
+  );
 }
+
--- src/components/Reviews.tsx
+++
@@ -1,8 +0,0 @@
-const Product = async() => {
-    await new Promise(resolve => setTimeout(resolve, 4000));
-    return (
-        <h1>Products</h1>
-    )
-}
-
-export default Product;
