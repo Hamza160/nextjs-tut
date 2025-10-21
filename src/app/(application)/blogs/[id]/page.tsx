c03cd08d5ac1c3e71a33cf3693572a88109a9d2e
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-21 11:56:41 +0500

    Static Site Generation

--- src/app/(application)/blogs/[id]/page.tsx
+++ src/app/(application)/blogs/[id]/page.tsx
@@ -1,21 +1,32 @@
-import type { Metadata } from 'next';
+import type {Metadata} from 'next';
 import React from 'react';
 import {notFound} from "next/navigation";
-import {isNumber} from "node:util";
-export async function generateMetadata({params}: {params:Promise<{id:string}>}): Promise<Metadata>{
+
+export async function generateMetadata({params}: { params: Promise<{ id: string }> }): Promise<Metadata> {
     const {id} = await params
     return {
         title: `Blog ${id}`,
     }
 }
 
-export default async function BlogDetails({params}:{params:Promise<{id:string}>}) {
+export async function generateStaticParams() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+
+    return data.map((item: { id: string; }) => {
+        return {
+            id: String(item.id),
+        }
+    })
+}
+
+export default async function BlogDetails({params}: { params: Promise<{ id: string }> }) {
     const {id} = await params
-    if(!/^\d+$/.test(id)){
+    if (!/^\d+$/.test(id)) {
         notFound();
     }
     return (
-    <div>Blog Details for {id}</div>
-  );
+        <div>Blog Details for {id}</div>
+    );
 }
 
